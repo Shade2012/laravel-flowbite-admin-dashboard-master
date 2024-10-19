76ad2872d242a0a6b9b6fdae8f2ac203c1a8e563
Syntax0642 <calvinapriliohariyanto04@gmail.com>
2024-10-19 16:03:03 +0700

    fix [CA] bug undefined room

--- resources/views/admin/ruang/delete.blade.php
+++ resources/views/admin/ruang/delete.blade.php
@@ -19,7 +19,7 @@
                 </div>
                 <!-- Modal body -->
                 <div class="p-6 pt-0 text-center">
-                    <form action="{{ route('admin.ruang.destroy', ['ruang' => $lesson->id]) }}" method="POST" enctype="multipart/form-data">
+                    <form action="{{ route('admin.ruang.destroy', ['ruang' => $room->id]) }}" method="POST" enctype="multipart/form-data">
                         @method('DELETE')
                         @csrf
                         <svg class="w-16 h-16 mx-auto text-red-600" fill="none" stroke="currentColor"
@@ -28,14 +28,14 @@
                                   d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                         </svg>
                         <h3 class="mt-5 mb-6 text-lg text-gray-500 dark:text-gray-400">Are you sure you want to delete
-                            {{ $rooom->nama_ruang }}?</h3>
+                            {{ $room->nama_ruang }}?</h3>
                         <button type="submit"
                                 class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-base inline-flex items-center px-3 py-2.5 text-center mr-2 dark:focus:ring-red-800">
                             Yes, I'm sure
                         </button>
                         <button type="button"
                                 class="text-gray-900 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-primary-300 border border-gray-200 font-medium inline-flex items-center rounded-lg text-base px-3 py-2.5 text-center dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-                                data-modal-toggle="delete-pelajaran-modal{{ $rooom->id }}">
+                                data-modal-toggle="delete-pelajaran-modal{{ $room->id }}">
                             No, cancel
                         </button>
                     </form>
